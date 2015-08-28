360af8c0c13aaad52e7aea3c5e8e2f596fad683c
Bas van der Hoek <bvdhoek@zilverline.com>
2015-08-28 15:57:03 +0200

    Initial Commit

---
+++ Markdrive/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Markdrive
+//
+//  Created by Bas van der Hoek on 28/08/15.
+//  Copyright (c) 2015 Bas van der Hoek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
