8a5ad6a43a829a3219e568a73e8d58c3c43467ec
Bas van der Hoek <bvdhoek@zilverline.com>
2015-08-30 17:48:56 +0200

    use keychain for tests

--- MarkdriveTests/MarkdriveTests.swift
+++ MarkdriveTests/MarkdriveTests.swift
@@ -3,68 +3,37 @@ import XCTest
 
 class MarkdriveTests: XCTestCase {
 
-    private let kKeychainItemName = "Drive API"
+    private let kKeychainItemName = "Markdrive API"
     private let kClientID = "41088727215-i68simor9c7ianupie1f0f8hnev2a6nk.apps.googleusercontent.com"
     private let kClientSecret = "l1sIRaNJ88g8kJtyC-jzArKF"
     
-    func testApiKey() {
+    func testKeychainOAuth() {
         let readyExpectation = expectationWithDescription("ready")
         
         let service: GTLServiceDrive = GTLServiceDrive()
-        service.APIKey = "AIzaSyDkqpkC7mHUnH97Vc5d65yPYItbgHZ3jEk"
-        let query = GTLQueryDrive.queryForFilesList() as! GTLQueryDrive
-        service.executeQuery(query, completionHandler: { (ticket, result, error) -> Void in
-            if let error = error {
-                println(error.localizedDescription)
-                return
-            }
-            
-            let files = result as? GTLDriveFileList
-//            if let items = files!.items() where !items.isEmpty {
-//                for file in items as! [GTLDriveFile] {
-//                    println("\(file.title) (\(file.identifier))")
-//                }
-//            } else {
-//                println("No files found.")
-//            }
-            readyExpectation.fulfill()
-        })
-        
-        waitForExpectationsWithTimeout(5.0, handler:nil)
-    }
-    
-    func testOAuth() {
-        let readyExpectation = expectationWithDescription("ready")
-        GTMOAuth2WindowController.authForGoogleFromKeychainForName(
+        service.authorizer = GTMOAuth2WindowController.authForGoogleFromKeychainForName(
             kKeychainItemName,
             clientID: kClientID,
             clientSecret: kClientSecret
         )
-        
-        
-        let service: GTLServiceDrive = GTLServiceDrive()
         let query = GTLQueryDrive.queryForFilesList() as! GTLQueryDrive
         service.executeQuery(query, completionHandler: { (ticket, result, error) -> Void in
             if let error = error {
-                println(error.localizedDescription)
-                return
+                XCTFail(error.localizedDescription)
             }
             
             let files = result as? GTLDriveFileList
-            //            if let items = files!.items() where !items.isEmpty {
-            //                for file in items as! [GTLDriveFile] {
-            //                    println("\(file.title) (\(file.identifier))")
-            //                }
-            //            } else {
-            //                println("No files found.")
-            //            }
+            if let items = files!.items() where !items.isEmpty {
+                for file in items as! [GTLDriveFile] {
+                    println("\(file.title) (\(file.identifier))")
+                }
+            } else {
+                println("No files found.")
+            }
             readyExpectation.fulfill()
         })
         
-
-        
         waitForExpectationsWithTimeout(5.0, handler:nil)
-
     }
     
 }
